d641bb588fedae05c05b2b035fcdc71b07951908
Joseph <softjoe40@gmail.com>
2022-05-04 10:42:44 +0100

    finish Partner.js, done with header page

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,60 +11,63 @@ import "../styles/header.css"
 
 export default function Header() {
     return (
-        <div className="div1">
-            <nav className="nav-header">
-                <div className="div2">
-                    <div className="logo-div">
-                        <span className="img-span"><img className="logo" src={Logo} alt="logo" /></span>
+        <>
+            <div className="div1">
+                <nav className="nav-header">
+                    <div className="div2">
+                        <div className="logo-div">
+                            <span className="img-span"><img className="logo" src={Logo} alt="logo" /></span>
 
-                        <p>Artic Travels</p>
+                            <p>Artic Travels</p>
+                        </div>
+                        <div className="nav-ul">
+                            <ul className="nav-list">
+                                <li>About Us</li>
+                                <li>Support</li>
+                                <li>Language</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="nav-ul">
-                        <ul className="nav-list">
-                            <li>About Us</li>
-                            <li>Support</li>
-                            <li>Language</li>
-                        </ul>
-                    </div>
+                    <button className="sign-in">Sign In</button>
+                </nav>
+                <div className="div3">
+                    <p className="plan">Plan the perfect winter trip</p>
+                    <p className="more-plan">Easily plan your ideal ski trip from home with the help of professionals</p>
+                    <button className="book-here">Book Here</button>
                 </div>
-                <button className="sign-in">Sign In</button>
-            </nav>
-            <div className="div3">
-                <p className="plan">Plan the perfect winter trip</p>
-                <p className="more-plan">Easily plan your ideal ski trip from home with the help of professionals</p>
-                <button className="book-here">Book Here</button>
+                <div className="overflow">
+                    <div className="overflow-div1">
+                        <div>
+                            <img src={LocationLogo} alt="location-img" />
+                            <p>LOCATION</p>
+                        </div>
+                        <p><span className="span">Iceland</span><img src={ArrowDownLogo} alt="arrow-down-logo" /></p>
+                    </div>
+                    <div className="overflow-div2">
+                        <div>
+                            <img src={PersonLogo} alt="location-img" />
+                            <p>PERSONS</p>
+                        </div>
+                        <p><span className="span">4 Persons</span><img src={ArrowDownLogo} alt="arrow-down-logo" /></p>
+                    </div>
+                    <div className="overflow-div3">
+                        <div>
+                            <img src={CalenderLogo} alt="check in" />
+                            <p>CHECK IN</p>
+                        </div>
+                        <p><span className="span">12 January 2022</span><img src={ArrowDownLogo} alt="arrow-down-logo" /></p>
+                    </div>
+                    <div className="overflow-div4">
+                        <div>
+                            <img src={CalenderLogo} alt="check in" />
+                            <p>CHECK OUT</p>
+                        </div>
+                        <p><span className="span">18 January 2022</span><img src={ArrowDownLogo} alt="arrow-down-logo" /></p>
+                    </div>
+                    <button className="book">Book trip<img src={BookHerePlane} alt="book" /></button>
+                </div>
             </div>
-            <div className="overflow">
-                <div className="overflow-div1">
-                    <div>
-                        <img src={LocationLogo} alt="location-img" />
-                        <p>LOCATION</p>
-                    </div>
-                    <p><span className="span">Iceland</span><img src={ArrowDownLogo} alt="arrow-down-logo" /></p>
-                </div>
-                <div className="overflow-div2">
-                    <div>
-                        <img src={PersonLogo} alt="location-img" />
-                        <p>PERSONS</p>
-                    </div>
-                    <p><span className="span">4 Persons</span><img src={ArrowDownLogo} alt="arrow-down-logo" /></p>
-                </div>
-                <div className="overflow-div3">
-                    <div>
-                        <img src={CalenderLogo} alt="check in" />
-                        <p>CHECK IN</p>
-                    </div>
-                    <p><span className="span">12 January 2022</span><img src={ArrowDownLogo} alt="arrow-down-logo" /></p>
-                </div>
-                <div className="overflow-div4">
-                    <div>
-                        <img src={CalenderLogo} alt="check in" />
-                        <p>CHECK OUT</p>
-                    </div>
-                    <p><span className="span">18 January 2022</span><img src={ArrowDownLogo} alt="arrow-down-logo" /></p>
-                </div>
-                <button className="book">Book trip<img src={BookHerePlane} alt="book" /></button>
-            </div>
-        </div>
+
+        </>
     )
 }
--- src/components/Partners.js
+++ src/components/Partners.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import NikeLogo from "../images/Group06.svg"
+import StarLogo from "../images/Group07.svg"
+import AppleLogo from "../images/apple-pay-logo-svgrepo-com 106.svg"
+import IntraxLogo from "../images/Group08.svg"
+import MastercardLogo from "../images/mastercard-2-logo-svgrepo-com 109.svg"
 import "../styles/Partners.css"
 
 function Partners() {
-  return (
-      <div className='partners'>
-          
-    </div>
-  )
+    return (
+        <div className='partners'>
+            <div><img src={NikeLogo} alt="logo" /></div>
+            <div><img src={AppleLogo} alt="logo" /></div>
+            <div><img src={StarLogo} alt="logo" /></div>
+            <div><img src={IntraxLogo} alt="logo" /></div>
+            <div><img src={MastercardLogo} alt="" /></div>
+        </div>
+    )
 }
 
 export default Partners
